5aca92c8e46cca0c3c9bd179f7aeb818507dfc45
syabana.wahyudi <syabana.wahyudi@gmail.com>
2021-05-25 10:58:09 +0700

    init commit

---
+++ src/main/java/org/bana/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package org.bana.employeemanager.repo;
+
+import org.bana.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/main/java/org/bana/employeemanager/service/EmployeeService.java
@@ -0,0 +1,41 @@
+package org.bana.employeemanager.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.bana.employeemanager.exception.UserNotFoundException;
+import org.bana.employeemanager.model.Employee;
+import org.bana.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteById(id);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findById(id).orElseThrow(() -> new UserNotFoundException("fdfdfdf"));
+    }
+}
